ace294d281da43327b73cb7f265917427c8c0da8
Frederick Full <frederick.full@posteo.de>
2020-11-23 13:08:45 +0000

    violentmonkey test

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # sigma8
+
+This is a userscript.
+
+## Development
+
+``` sh
+$ yarn dev
+```
+
+## Building
+
+```sh
+$ yarn build
+```
+
+## Lint
+
+``` sh
+$ yarn lint
+```
---
+++ src/meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        My Script
+// @namespace   Violentmonkey Scripts
+// @description This is a userscript.
+// @match       *://*/*
+// @grant       none
+// @version     process.env.VERSION
+// @author      -
+// @require     https://cdn.jsdelivr.net/combine/npm/@violentmonkey/dom@1,npm/@violentmonkey/ui@0.4
+// ==/UserScript==
